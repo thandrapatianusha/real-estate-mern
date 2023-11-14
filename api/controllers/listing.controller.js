753a0f5791dc3d19b191eb365670dc99e3a007ea
Anilkumar052000 <anilkumarlucky2000@gmail.com>
2023-11-14 20:42:02 +0530

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 export const createListing = async (req, res, next) => {
     try{
 const listing = await Listing.create(req.body);
@@ -7,3 +8,21 @@ return res.status(201).json(listing);
         next(error);
     }
 }
+
+export const deleteListing = async(req, res, next) => {
+const listing = await Listing.findById(req.params.id);
+
+if(!listing){
+    return next(errorHandler(404, 'Listing not found'));
+}
+
+if(req.user.id !== listing.userRe){
+    return next(errorHandler(401, 'You can only delete your own listings!'));
+}
+try{
+await Listing.findByIdAndDelete(req.params.id);
+res.status(200).json('listing has been deleted');
+} catch(error) {
+    next(error);
+}
+};
