c5964ff32815ac9f8c496b870661cdabd3929b4c
Anilkumar052000 <anilkumarlucky2000@gmail.com>
2023-11-15 21:42:27 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -26,3 +26,21 @@ res.status(200).json('listing has been deleted');
     next(error);
 }
 };
+
+export const updateListing = async(req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    
+    if(!listing){
+        return next(errorHandler(404, 'Listing not found'));
+    }
+    
+    if(req.user.id !== listing.userRe){
+        return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+    try{
+    const updatedListing=await Listing.findByIdAndUpdate(req.params.id, req.body, {new : true});
+    res.status(200).json(updatedListing);
+    } catch(error) {
+        next(error);
+    }
+    };
